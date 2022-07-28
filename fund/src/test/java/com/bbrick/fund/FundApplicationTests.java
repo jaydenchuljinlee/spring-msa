760571012dd9e2919ebe2742c37bd219d8680a93
cheojlin <cheoljin1234@naver.com>
2022-07-28 17:02:01 +0900

    added fund server

---
+++ fund/src/test/java/com/bbrick/fund/FundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbrick.fund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
