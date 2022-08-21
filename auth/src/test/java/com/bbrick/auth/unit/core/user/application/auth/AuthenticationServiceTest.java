7f6aeaf18146bfaa05d001be5f2046d17a89259c
cheojlin <cheoljin1234@naver.com>
2022-08-21 19:50:24 +0900

    added service unit test files

---
+++ auth/src/test/java/com/bbrick/auth/unit/core/user/application/auth/AuthenticationServiceTest.java
@@ -0,0 +1,7 @@
+package com.bbrick.auth.unit.core.user.application.auth;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("AuthenticationService 클래스")
+public class AuthenticationServiceTest {
+}
